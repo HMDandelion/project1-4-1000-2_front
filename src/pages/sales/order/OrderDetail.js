e415d67172ab849bbc74321239ad7ad3d2af601c
ppyye <6076hs@gmail.com>
2024-06-12 14:45:45 +0900

    #20 feature: implementing order detail view

--- src/pages/sales/order/OrderDetail.js
+++ src/pages/sales/order/OrderDetail.js
@@ -4,9 +4,8 @@ import {
     useColorModeValue,
     Heading,
     Flex,
-    Button,
     useDisclosure,
-    Divider
+    Divider, GridItem, Center, Grid, VStack
 } from "@chakra-ui/react";
 
 import {useDispatch, useSelector} from "react-redux";
@@ -14,9 +13,9 @@ import React, {useEffect, useState} from "react";
 import {useLocation, useNavigate, useParams} from "react-router-dom";
 import AgGrid from "../../../components/table/AgGrid";
 import Card from "../../../components/card/Card";
-import DeleteAlertButton from "../../../components/button/DeleteAlertButton";
-import {callEstimateAPI} from "../../../apis/EstimateAPICalls";
 import {callOrderAPI} from "../../../apis/OrderAPICalls";
+import OrderStatusProgress from "./OrderStatusProgress";
+
 
 
 function OrderDetail() {
@@ -58,20 +57,107 @@ function OrderDetail() {
         return new Intl.NumberFormat('ko-KR').format(number);
     };
 
+    const getDayLeft = (deadline) => {
+        const deadlineDate = new Date(deadline);
+        const today = new Date();
+        const daysLeft = Math.ceil((deadlineDate.getTime() - today.getTime()) / (1000 * 60 * 60 * 24));
+
+        return (
+            <Text color={daysLeft < 3 ? 'red' : ''}>{daysLeft < 0 ? 0 : daysLeft} 일</Text>
+        );
+    }
+
     return (
         order &&
         <>
-            <Flex justify='space-between'>
-                <Text fontSize='3xl' fontWeight='800' color={textColor} m='10px'>
-                    주문 상세
-                </Text>
-                <div>
-                    <Button colorScheme='gray' size='xs' onClick={onOpen}>
-                        수정
-                    </Button>
-                    <DeleteAlertButton/>
-                </div>
-            </Flex>
+            <Grid templateColumns='repeat(15, 1fr)'>
+                <GridItem colSpan={7}>
+                    <VStack
+                        spacing={2}
+                        align='stretch'
+                    >
+                        <Flex justify='space-between'>
+                            <Text fontSize='xl' fontWeight='800' color={textColor} m='10px'>
+                                주문 상세
+                            </Text>
+                            <Text fontSize='sm' fontWeight='600' color={textColor} mt='15px' mr='10px'>
+                                주문번호 : {order.orderCode}
+                            </Text>
+                        </Flex>
+                        <Divider/>
+                        <OrderStatusProgress/>
+                        <Flex justify='space-between'>
+                            <Text fontSize='md' fontWeight='700' color={textColor} m='5px 10px'>
+                                주문일자
+                            </Text>
+                            <Text fontSize='md' fontWeight='500' color={textColor} m='5px 10px'>
+                                {order.orderDatetime}
+                            </Text>
+                        </Flex>
+                        <Flex justify='space-between'>
+                            <Text fontSize='md' fontWeight='700' color={textColor} m='5px 10px'>
+                                마감일자
+                            </Text>
+                            <Text fontSize='md' fontWeight='500' color={textColor} m='5px 10px'>
+                                {order.deadline}
+                            </Text>
+                        </Flex>
+                        <Flex justify='flex-end'>
+                            {
+                                order.status === 'COMPLETED' ?
+                                    <>
+                                        <Badge fontSize='md' fontWeight='700' colorScheme='green' my='8px'>
+                                            👏 완료일시
+                                        </Badge>
+                                        <Text fontSize='md' fontWeight='500' color={textColor} m='10px'>
+                                            {order.completedAt}
+                                        </Text>
+                                    </> :
+                                    <>
+                                        <Text fontSize='md' fontWeight='700' color={textColor} m='10px'>
+                                            마감까지
+                                        </Text>
+                                        <Text fontSize='md' fontWeight='500' color='orange.600' m='10px'>
+                                            {getDayLeft(order.deadline)}
+                                        </Text>
+                                    </>
+                            }
+                        </Flex>
+                    </VStack>
+                </GridItem>
+                <GridItem >
+                    <Center height='260px'>
+                        <Divider orientation='vertical'/>
+                    </Center>
+                </GridItem>
+                <GridItem colSpan={7}>
+                    <VStack
+                        align='stretch'
+                    >
+                        <Flex>
+                            <Text fontSize='xl' fontWeight='800' color={textColor} m='10px'>거래처 상세</Text>
+                        </Flex>
+                        <Divider/>
+                        <Flex>
+                            <Text fontSize='xl' fontWeight='700' color={textColor} m='10px'>{order.client.clientName}</Text>
+                        </Flex>
+                        <Flex justify='space-between'>
+                            <Text fontSize='md' fontWeight='700' color={textColor} m='5px 10px'>대표명</Text>
+                            <Text fontSize='md' fontWeight='500' color={textColor} m='5px 10px'>{order.client.representativeName}</Text>
+                        </Flex>
+                        <Flex justify='space-between'>
+                            <Text fontSize='md' fontWeight='700' color={textColor} m='5px 10px'>연락처</Text>
+                            <Text fontSize='md' fontWeight='500' color={textColor} m='5px 10px'>{order.client.phone}</Text>
+                        </Flex>
+                        <Flex justify='space-between'>
+                            <Text fontSize='md' fontWeight='700' color={textColor} m='5px 10px'>주소</Text>
+                            <Text fontSize='md' fontWeight='500' color={textColor} m='5px 10px'>{order.client.address}{order.client.addressDetail}</Text>
+                        </Flex>
+                    </VStack>
+                </GridItem>
+            </Grid>
+
+
 
 
             <Divider mt='20px'/>
