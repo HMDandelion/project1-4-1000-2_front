13b724d40a2788e267ba7945278e02444aedf34e
MIdWit0828 <kindj9897@gmail.com>
2024-06-14 19:21:19 +0900

    [FEATURE] error fix2

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd ../
 mkdir output
-cp -R ./[HMDandelion-project1-4-1000-2_front-project1-4-1000-2_front]/* ./output
-cp -R ./output ./[HMDandelion-project1-4-1000-2_front-project1-4-1000-2_front]/
+cp -R ./[project1-4-1000-2_front]/* ./output
+cp -R ./output ./[project1-4-1000-2_front]/
